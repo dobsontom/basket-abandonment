db2c00e39b390c3f3f330720e6ebe16e918bc206
dobsontxx <dobs.tx@gmail.com>
2024-08-20 10:41:41 +0100

    Update table name, add ERROR fail state to duplication check

--- queries/tests.sql
+++ queries/tests.sql
@@ -2,7 +2,7 @@
 SELECT
    COUNT(*) AS missing_email_count
 FROM
-   `basket-abandonment.basket_abandonment.actionable_dataset`
+   `basket-abandonment.basket_abandonment.basket_abandonment`
 WHERE
    email IS NULL;
 
@@ -10,12 +10,14 @@ WHERE
 -- Check for duplicate user_pseudo_id and event_timestamp pairs
 SELECT
    user_pseudo_id,
-   event_timestamp,
-   COUNT(*) AS record_count
+   abandon_timestamp,
+   COUNT(*) AS record_count,
+   CASE
+      WHEN COUNT(*) > 1 THEN ERROR('Potential Duplication') AS ERROR(error_message)
+      ELSE 'No Duplication'
+   END AS error_on_duplicate
 FROM
-   `basket-abandonment.basket_abandonment.actionable_dataset`
+   `basket-abandonment.basket_abandonment.basket_abandonment`
 GROUP BY
    user_pseudo_id,
-   event_timestamp
-HAVING
-   record_count > 1;
+   abandon_timestamp
